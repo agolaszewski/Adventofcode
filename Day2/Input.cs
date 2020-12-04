ed0297fea35bd1f98078c5b4fcfe6b961663654e
Andrzej Golaszewski <andrzej.golaszewski@taxsystems.com>
2020-12-05 00:06:20 +0100

    day2

---
+++ Day2/Input.cs
@@ -0,0 +1,13 @@
+﻿namespace Day2
+{
+    public class Input
+    {
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        public char CharToValidate { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Parser;
+
+namespace Day2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var parser = new InputReader();
+            List<Input> inputs = parser.Read<Input>("input.txt", line =>
+            {
+                //1-3 a: abcde
+                var array = line.Split(' ');
+                var minMaxArray = array[0].Split('-');
+
+                return new Input()
+                {
+                    Min = int.Parse(minMaxArray[0]),
+                    Max = int.Parse(minMaxArray[1]),
+                    CharToValidate = array[1][0],
+                    Password = array[2]
+                };
+            });
+
+            int validPassword = 0;
+            foreach (var input in inputs)
+            {
+                int charInPassword = input.Password.Count(x => x == input.CharToValidate);
+                if (charInPassword >= input.Min && charInPassword <= input.Max)
+                {
+                    validPassword++;
+                }
+            }
+
+            Console.WriteLine(validPassword);
+
+            validPassword = 0;
+            foreach (var input in inputs)
+            {
+                if (input.Password[input.Min - 1] == input.CharToValidate ^ input.Password[input.Max - 1] == input.CharToValidate)
+                {
+                    validPassword++;
+                }
+            }
+
+            Console.WriteLine(validPassword);
+        }
+    }
+}
