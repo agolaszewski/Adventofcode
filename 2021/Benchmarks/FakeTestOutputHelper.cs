15f5a413a52de2851988b97a0eef8b907abb341a
Andrzej Gołaszewski <agolaszewski>
2021-12-06 01:36:55 +0100

    fancy stuff

---
+++ 2021/Benchmarks/FakeTestOutputHelper.cs
@@ -0,0 +1,15 @@
+﻿using Xunit.Abstractions;
+
+namespace Benchmarks
+{
+    public class FakeTestOutputHelper : ITestOutputHelper
+    {
+        public void WriteLine(string message)
+        {
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+        }
+    }
+}
