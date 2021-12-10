523a1b9d103ecf139297262cc2a282baf6d39401
Andrzej Gołaszewski <agolaszewski>
2021-12-10 10:10:00 +0100

    day 10

---
+++ 2021/Y2021/Day10/Part1.cs
@@ -0,0 +1,63 @@
+﻿using Parser;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Y2021.Day10
+{
+    public class Part1
+    {
+        private readonly ITestOutputHelper _console;
+
+        public Part1(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            var input = InputReader.Read("Day10.txt", line => line);
+            var pairs = new Dictionary<char, char>()
+            {
+                { '[', ']' },
+                { '(', ')' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            var score = new Dictionary<char, int>()
+            {
+                { ']', 57 },
+                { ')', 3 },
+                { '}', 1197 },
+                { '>', 25137 },
+            };
+
+            var sum = 0;
+            foreach (var line in input)
+            {
+                var array = line.ToCharArray();
+                var stack = new Stack<char>();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (pairs.ContainsKey(array[i]))
+                    {
+                        stack.Push(array[i]);
+                    }
+                    else
+                    {
+                        var pop = stack.Pop();
+                        if (pairs[pop] != array[i])
+                        {
+                            sum += score[array[i]];
+                            break;
+                        }
+                    }
+                }
+            }
+            _console.WriteLine(sum.ToString());
+        }
+    }
+}
---
+++ 2021/Y2021/Day10/Part2.cs
@@ -0,0 +1,78 @@
+﻿using Parser;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Y2021.Day10
+{
+    public class Part2
+    {
+        private readonly ITestOutputHelper _console;
+
+        public Part2(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            var input = InputReader.Read("Day10.txt", line => line);
+            var pairs = new Dictionary<char, char>()
+            {
+                { '[', ']' },
+                { '(', ')' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            var score = new Dictionary<char, long>()
+            {
+                { ']', 2 },
+                { ')', 1 },
+                { '}', 3 },
+                { '>', 4 },
+            };
+
+            List<long> result = new List<long>();
+            foreach (var line in input)
+            {
+                var array = line.Select(x => x).ToList();
+                var stack = new Stack<char>();
+                var isCorrupted = false;
+
+                for (int i = 0; i < array.Count; i++)
+                {
+                    if (pairs.ContainsKey(array[i]))
+                    {
+                        stack.Push(array[i]);
+                    }
+                    else
+                    {
+                        var pop = stack.Pop();
+                        if (pairs[pop] != array[i])
+                        {
+                            isCorrupted = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!isCorrupted)
+                {
+                    long sum = 0;
+                    while (stack.Count > 0)
+                    {
+                        var pop = stack.Pop();
+                        sum = sum * 5 + score[pairs[pop]];
+                    }
+                    result.Add(sum);
+                }
+            }
+
+            var answer = result.OrderBy(x => x).Skip(result.Count / 2).First().ToString();
+            _console.WriteLine(answer);
+        }
+    }
+}
